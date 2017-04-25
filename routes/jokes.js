4c0f33fe31f2528f05c11b34d161168d06d38ea4
fissan88 <jeppelindstromjensen@gmail.com>
2017-04-25 12:58:39 +0200

    2nd commit

--- routes/jokes.js
+++ routes/jokes.js
@@ -1,31 +1,47 @@
-var express = require('express');
-var router = express.Router();
+module.exports = function(express) {
+    var Joke = require('../models/joke');
+    var router = express.Router();
 
-router.route('/api/jokes')
-    .get(function(req, res) {
-        var jokes;
-        //MongoDB kald, hent alles jokezz
-        jokes.find(function (err,msg) {
+        router.route('/api/jokes')
+                .get(function(req, res) {
+                    //MongoDB kald, henter alles jokezz
+                    Joke.find({}, function(err, jokes) {
+                            if(err) {
+                                console.log("Error getting jokes...")
+                            } else {
+                                res.json(jokes);
+                            }
+                        });
+                })
+                .post(function(req, res) {
+                    //Create joke
+                    var newJoke = new Joke({
+                        setup: req.body.setup,
+                        punchline: req.body.punchline
+                    });
+
+                    //Post enkelt joke til mongozzzz
+                    newJoke.save(function (err) {
+                        if (err) {
+                            res.status(500);
+                            res.send(err);
+                        } else {
+                            res.json({msg: "message saved!"});
+                        }
+                    });
+        });
+
+    router.get('/api/jokes/:id', function(req, res, next) {
+        //Hent joke med bestemt :id
+        var id = req.params.id;
+        Joke.find({_id: id},function (err,joke) {
             if(err){
                 res.send(err);
             }
             else{
-                res.json(msg);
+                res.json(joke);
             }
         })
-    })
-    .post(function(req, res) {
-        //Create joke
-        var joke = new Joke({
-            setup: req.body.setup,
-            punchline: req.body.punchline
-         //Post enkelt joke til mongozzzz
-});
-
-router.get('/api/jokes/:id', function(req, res, next) {
-    //Hent joke med bestemt :id
-});
-
-
-
-module.exports = router;
+    });
+return router;
+};
