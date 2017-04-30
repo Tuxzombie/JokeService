6dd9485a5d41abbd71192f1286abc0bd2cef5028
fissan88 <jeppelindstromjensen@gmail.com>
2017-04-30 09:32:54 +0200

    3rd commit

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,17 @@ var request = require('request');
 var Promise = require('promise');
 var otherJokeServices = [];
 var otherJokes = [];
-global.otherJokes = otherJokes;
+
+var registerServerToJokeRegistry  = function() {
+    request.post({
+        url:     'https://krdo-joke-registry.herokuapp.com/api/services',
+        form:    { "name": "Dad's Jokeservice",
+                    "address": "http://jokeService-jeppe-steen.herokuapp.com",
+                    "secret": "1234" }
+    }, function(error, response, body){
+        console.log(error);
+    });
+};
 
 var requestServers = function() {
     var options = {json: true, url: 'https://krdo-joke-registry.herokuapp.com/api/services'};
@@ -13,7 +23,9 @@ var requestServers = function() {
             console.log("Error getting other joke services.")
         } else {
             for (var i = 0; i < body.length; i++) {
-                otherJokeServices.push(body[i]);
+                if(body[i].address !== "http://jokeService-jeppe-steen.herokuapp.com") {
+                    otherJokeServices.push(body[i]);
+                }
             }
         }
         console.log("serverArray indeholder: "+otherJokeServices);
@@ -39,6 +51,7 @@ var populateJokeArray = function() {
                                     if (jokeBody[m].setup && jokeBody[m].punchline) {
                                         otherJokes.push(
                                             {
+                                                '_id' : jokeBody[m]._id,
                                                 'setup': jokeBody[m].setup,
                                                 'punchline': jokeBody[m].punchline,
                                                 'fromServer': options.url
@@ -55,77 +68,12 @@ var populateJokeArray = function() {
     }
 };
 
+registerServerToJokeRegistry();
 requestServers();
-
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
-    // var requestServers = function() {
-    //     var options = {json: true, url: 'https://krdo-joke-registry.herokuapp.com/api/services'};
-    //     request(options, function (error, response, body) {
-    //         console.log("Kommer vi ned i request????");
-    //         if (error) {
-    //             console.log("Error getting other joke services.")
-    //         } else {
-    //             for (var i = 0; i < body.length; i++) {
-    //                 otherJokeServices.push(body[i]);
-    //                 console.log("Pushed vi servere?");
-    //             }
-    //         }
-    //     });
-    // };
-
-    // var populateJokeArr = function() {
-    //     for (var i = 0; i < otherJokeServices.length; i++) {
-    //         options = {json: true, url: body[i].address + 'api/jokes'};
-    //         console.log(otherJokeServices[i].address);
-    //
-    //         request(options, function (error2, response, jokeBody) {
-    //             if (error2) {
-    //                 console.log("Invalid url")
-    //             } else if (response.statusCode == 200) {
-    //                 console.log('Responded with statuscode: ' + response.statusCode);
-    //                 // console.log('Fetching jokes from server URL: ' + otherJokeServices[i].address);
-    //                 for (var m = 0; m < jokeBody.length; m++) {
-    //                     if (jokeBody[m].setup && jokeBody[m].punchline) {
-    //                         otherJokes.push(
-    //                             {
-    //                                 'setup': jokeBody[m].setup,
-    //                                 'punchline': jokeBody[m].punchline,
-    //                                 'fromServer': otherJokeServices[i].address
-    //                             });
-    //                     }
-    //                 }
-    //                 console.log(otherJokes);
-    //
-    //             } else {
-    //                 console.log('Responded with statuscode: ' + response.statusCode)
-    //             }
-    //         });
-    //     }
-    // };
-
-    // function requestp() {
-    //     return new Promise(function (resolve, reject) {
-    //         var options = {json: true, url: 'https://krdo-joke-registry.herokuapp.com/api/services'};
-    //         request(options, function (error, response, body) {
-    //             if (error) {
-    //                 console.log("Error getting other joke services.")
-    //             } else {
-    //                 for (var i = 0; i < body.length; i++) {
-    //                     otherJokeServices.push(body[i]);
-    //                 }
-    //             }
-    //         });
-    //     });
-    // }
-    //
-    // requestp().then(populateJokeArr(), function (err) {
-    //     console.error("%s; %s", err.message, url);
-    //     console.log("%j", err.res.statusCode);
-    // });
-
+    requestServers();
     console.log(otherJokes);
 
     res.render('index', {
@@ -134,5 +82,7 @@ router.get('/', function (req, res, next) {
         punchline: '... So why couldn\'t the bicycle stand up by itself? It was two tired.'
     });
 });
+
+global.otherJokes = otherJokes;
 
 module.exports = router;
