8cdf59eecc61db7c9b37282ee436983c33434512
fissan88 <jeppelindstromjensen@gmail.com>
2017-04-30 11:46:27 +0200

    3rd commit

---
+++ models/joke.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Jeppe on 25-04-2017.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var Joke = new Schema({
+    setup: String,
+    punchline: String,
+    fromServer: String
+},  {collection: 'joke', versionKey: false});
+
+module.exports = mongoose.model('joke', Joke);
